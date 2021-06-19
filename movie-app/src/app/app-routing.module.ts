e60249ab27546754781d346b2c1ccdbfaa32e987
Mohammad Hassan <Mohammad.Hassan3@in.ey.com>
2021-06-19 10:01:09 +0530

    implement lazy loading

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movielist', loadChildren:()=>import('../app/movie-list/movie-list.module').then(x=>x.MovieListModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- movie-app/src/app/movie-list/movie-list.module.ts
+++ movie-app/src/app/movie-list/movie-list.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { RoutingModule } from './movie-list-routing.module';
 
 @NgModule({
   declarations: [MovieListComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RoutingModule
+  ],
+  exports:[MovieListComponent],
+  entryComponents:[MovieListComponent]
 })
 export class MovieListModule { }
